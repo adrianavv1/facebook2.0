bebc9a02cdfc09c7679ffcf6582433ee89d50c45
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-06-18 13:30:15 -0400

    adding facebook logo & next.config file

--- facebook2.0/components/Header.js
+++ facebook2.0/components/Header.js
@@ -1,9 +1,25 @@
+import Image from "next/image";
+
 function Header() {
-    return (
-        <div>
-        <h1>Header</h1>   
-        </div>
-    )
+  return (
+    <div>
+      <h1>Header</h1>
+
+      {/* Left */}
+      <div>
+        <Image
+          src="https://links.papareacr.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+      </div>
+
+      {/* Center */}
+
+      {/* Right */}
+    </div>
+  );
 }
 
 export default Header;
